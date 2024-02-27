92c7911354a17aae405aa18b7f7ea23b2a93a6a5
Lucas Neris <lucas.lima.neris@gmail.com>
2024-02-27 01:32:30 -0300

    refactor: added new services methods and some adjusts in DriverVehicle

--- driver-vehicle/service.go
+++ driver-vehicle/service.go
@@ -1,6 +1,12 @@
 package drivervehicle
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
+
+	"github.com/LucasMateus-eng/operations-service/driver"
+	"github.com/LucasMateus-eng/operations-service/vehicle"
+)
 
 type Service struct {
 	repo   Repository
@@ -12,4 +18,44 @@ func NewService(r Repository, l *slog.Logger) *Service {
 		repo:   r,
 		logger: l,
 	}
+}
+
+func (s *Service) GetByID(ctx context.Context, driverID, vehicleID int) (*DriverVehicle, error) {
+	driverVehicle, err := s.repo.GetByID(ctx, driverID, vehicleID)
+	if err != nil {
+		return nil, err
+	}
+
+	return driverVehicle, nil
+}
+
+func (s *Service) GetDriverListByVehicleID(ctx context.Context, specification *DriverVehicleSpectification) (*[]driver.Driver, error) {
+	drivers, err := s.repo.GetDriverListByVehicleID(ctx, specification)
+	if err != nil {
+		return nil, err
+	}
+
+	return drivers, nil
+}
+
+func (s *Service) GetVehicleListByDriverID(ctx context.Context, specification *DriverVehicleSpectification) (*[]vehicle.Vehicle, error) {
+	vehicles, err := s.repo.GetVehicleListByDriverID(ctx, specification)
+	if err != nil {
+		return nil, err
+	}
+
+	return vehicles, nil
+}
+
+func (s *Service) Create(ctx context.Context, dv *DriverVehicle) (*DriverVehicle, error) {
+	driverVehicle, err := s.repo.Create(ctx, dv)
+	if err != nil {
+		return nil, err
+	}
+
+	return driverVehicle, nil
+}
+
+func (s *Service) Delete(ctx context.Context, driverID, vehicleID int) error {
+	return s.repo.Delete(ctx, driverID, vehicleID)
 }
